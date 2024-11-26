4f292d454b75cb32c6e4ba2a0c53c46344c5cc95
NJERU MTWAITI <bernardnjerumtwaiti@gmail.com>
2024-11-26 07:19:50 -0500

    Update github-ssh.sh

--- scripts/github-ssh.sh
+++ scripts/github-ssh.sh
@@ -2,6 +2,8 @@
 
 ssh-keygen -t rsa -b 4096 -C "your_email@example.com"   #replace with your github email
 
+ eval $(ssh-agent -s) #start ssh agent
+  
 ssh-add ~/ssh/id_rsa    #path to your keys
 
 cat ~/.ssh/id_rsa.pub   #paste your public key to github account(settings)
